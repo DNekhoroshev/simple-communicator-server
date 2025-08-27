1e67d75794fe8165355a62b920db3a2275f17fa4
Dmitriy <dnechoroshev@gmail.com>
2025-08-27 14:40:06 +0300

    Initial commit

---
+++ src/main/java/ru/dnechoroshev/simplecommunicator/SimpleCommunicatorApplication.java
@@ -0,0 +1,13 @@
+package ru.dnechoroshev.simplecommunicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCommunicatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCommunicatorApplication.class, args);
+    }
+
+}
